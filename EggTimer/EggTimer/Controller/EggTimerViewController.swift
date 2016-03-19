2f665dadbf00e751d77951385a14e19a9dc40de6
domon <rd050201@gmail.com>
2016-03-19 18:31:09 +0900

    #5 ゆでたまごタイマー画面のVCでViewを読み込む処理を記述

--- EggTimer/EggTimer/Controller/EggTimerViewController.swift
+++ EggTimer/EggTimer/Controller/EggTimerViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 /// ゆでたまごタイマーの画面
 class EggTimerViewController: UIViewController {
-
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.view = EggTimerView.instantiateXib()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
