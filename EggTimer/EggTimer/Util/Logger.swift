2e08e39ea8248de66c3b2670fb277f60906f02b7
domon <rd050201@gmail.com>
2016-03-19 16:11:08 +0900

    #2 SwiftLintでワーニングが出ていた部分を修正　
    * デフォルトのコメントが長すぎてエラーだったため削除
    * デフォルトのメソッドが長すぎるので改行を挟むようにした
    * debug用のメソッドが長すぎたので改行を挟むようにした
    * ファイルの末尾の改行を1つにした

--- EggTimer/EggTimer/Util/Logger.swift
+++ EggTimer/EggTimer/Util/Logger.swift
@@ -10,14 +10,15 @@
 class Logger {
     
     /**
-    debug時のみ出力します
-    
-    - parameter obj:      ログ出力対象のオブジェクト
-    - parameter file:     ログ出力場所のファイル名
-    - parameter function: ログ出力場所のメソッド名
-    - parameter line:     ログ出力場所のファイルの行数
-    */
-    static func d(obj: AnyObject?, file: String = __FILE__, function: String = __FUNCTION__, line: Int = __LINE__) {
+     debug時のみ出力します
+     
+     - parameter obj:      ログ出力対象のオブジェクト
+     - parameter file:     ログ出力場所のファイル名
+     - parameter function: ログ出力場所のメソッド名
+     - parameter line:     ログ出力場所のファイルの行数
+     */
+    static func d(obj: AnyObject?, file: String = __FILE__,
+        function: String = __FUNCTION__, line: Int = __LINE__) {
             #if DEBUG
                 print("[\(file):\(function):\(line)] : \(obj)")
             #endif
